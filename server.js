47a7907db40997abc36a5ac770b496b5db520d4e
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-04-15 00:30:31 +0530

    server.js

--- server.js
+++ server.js
@@ -1,37 +1,27 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
+const socketIo = require("socket.io");
 const path = require("path");
-
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
-// Serve static files
+// app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
-
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+  console.log("a user connected");
 
   socket.on("join-room", (roomId) => {
     socket.join(roomId);
-    socket.to(roomId).emit("user-connected", socket.id);
+    console.log(`User joined room: ${roomId}`);
+  });
 
-    socket.on("offer", (data) => {
-      socket.to(roomId).emit("offer", data);
-    });
+  socket.on("signal", (data) => {
+    socket.to(data.roomId).emit("signal", data);
+  });
 
-    socket.on("answer", (data) => {
-      socket.to(roomId).emit("answer", data);
-    });
-
-    socket.on("ice-candidate", (data) => {
-      socket.to(roomId).emit("ice-candidate", data);
-    });
-
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
@@ -39,3 +29,45 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// const express = require("express");
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const path = require("path");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// // Serve static files
+// app.use(express.static(path.join(__dirname, "public")));
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected:", socket.id);
+
+//   socket.on("join-room", (roomId) => {
+//     socket.join(roomId);
+//     socket.to(roomId).emit("user-connected", socket.id);
+
+//     socket.on("offer", (data) => {
+//       socket.to(roomId).emit("offer", data);
+//     });
+
+//     socket.on("answer", (data) => {
+//       socket.to(roomId).emit("answer", data);
+//     });
+
+//     socket.on("ice-candidate", (data) => {
+//       socket.to(roomId).emit("ice-candidate", data);
+//     });
+
+//     socket.on("disconnect", () => {
+//       socket.to(roomId).emit("user-disconnected", socket.id);
+//     });
+//   });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
